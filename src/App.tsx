65c2080ce2f96e72ea207d54ba5dd15ad2a4f688
Tomasz John <t.john@e-con.pl>
2024-09-21 12:26:18 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  const onTagInputChange = (newValue: string) => {
+    console.log(newValue)
+  }
+
+  return (
+    <>
+      <div className='w-full'>
+        App
+      </div>
+    </>
+  )
+}
+
+export default App
